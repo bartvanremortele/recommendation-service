e54bf2a29dc993c958167090e6caa366fc7996d3
Nicolás Cornaglia <ncornag@gmail.com>
2016-12-08 22:22:53 +0200

    Added related product info & cache.

--- src/operations/recomendation.alsoviewed.js
+++ src/operations/recomendation.alsoviewed.js
@@ -7,19 +7,48 @@
  * @return {Function} The operation factory
  */
 function opFactory(base) {
+  const relatedLimit = base.config.get('relatedLimit');
+  const productInfoServiceName = base.config.get('productInfoServiceName');
   const op = {
-    handler: ({ productId }, reply) => {
+    cache: {
+      options: {
+        expiresIn: base.config.get('cache:alsoViewed')
+      },
+      name: 'alsoViewed',
+      keyGenerator: payload => payload.productId
+    },
+    handler: ({ productId, limit = relatedLimit }, reply) => {
       base.db.models.ViewsByProduct
-        .findOne({ _id: productId })
+        .findOne({ _id: productId }, {}, { lean: true })
         .exec()
         .then(doc => {
           if (!doc) {
             return reply(base.utils.genericResponse({ alsoViewed: [] }));
           }
-          return reply(base.utils.genericResponse({ alsoViewed: doc.related }));
-        })
-        .catch(error => reply(base.utils.genericResponse(null, error)));
 
+          const promises = doc.related.slice(0, limit).map(productData => {
+            return base.services
+              .call({
+                name: productInfoServiceName
+              }, {
+                id: productData.pid, fields: '-variants'
+              })
+              .catch(error => {
+                if (error.code && error.code !== 'product_not_found') base.logger.error(JSON.stringify(error));
+                return { error };
+              });
+          });
+          Promise
+            .all(promises)
+            .then(responses => {
+              return reply(base.utils.genericResponse({
+                alsoViewed: responses
+                  .filter(response => response.product)
+                  .map(response => response.product || {})
+              }));
+            })
+            .catch(error => reply(base.utils.genericResponse(null, error)));
+        });
     }
   };
   return op;
