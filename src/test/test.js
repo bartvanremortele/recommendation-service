87a9a12b4bf0aa2b80f262b40373a2fb50387a8d
Nicolás Cornaglia <ncornag@gmail.com>
2016-12-30 14:45:29 +0100

    Added Travis/Coveralls/Bithound processes and badges. Update dependencies. Added test skeleton.

---
+++ src/test/test.js
@@ -0,0 +1,14 @@
+const Code = require('code');
+const Lab = require('lab');
+const nock = require('nock');
+
+// shortcuts
+const lab = exports.lab = Lab.script();
+const describe = lab.describe;
+const beforeEach = lab.beforeEach;
+const after = lab.after;
+const it = lab.it;
+const expect = Code.expect;
+
+describe('Main', () => {
+});
