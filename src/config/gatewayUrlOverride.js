a3987cf16b6305ecafccb6540a44c46f86ce2d7c
Nicolás Cornaglia <ncornag@gmail.com>
2017-06-22 23:01:01 +0200

    Updated local service urls [skip ci].

--- src/config/gatewayUrlOverride.js
+++ src/config/gatewayUrlOverride.js
@@ -1,12 +1,14 @@
-function gatewayUrlOverrride(/* base */) {
+module.exports = (/* base */) => {
   const urls = {
     stock: 'http://localhost:3000',
+    cart: 'http://localhost:3001',
     catalog: 'http://localhost:3002',
     tax: 'http://localhost:3003',
     promotion: 'http://localhost:3004',
-    customer: 'http://localhost:3005'
+    customer: 'http://localhost:3005',
+    recommendation: 'http://localhost:3006',
+    payment: 'http://localhost:3007',
+    oauth: 'http://localhost:4000'
   };
   return (serviceName /* , serviceVersion, operationName */) => urls[serviceName];
-}
-
-module.exports = gatewayUrlOverrride;
+};
